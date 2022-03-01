2be7581a63e76786b215a14f22467c34cadcdc99
Ben Mason <locutus@the-collective.net>
2022-03-01 15:17:58 -0500

    Clean up of Resource Dumper

--- resource_dumper.py
+++ resource_dumper.py
@@ -1,32 +1,42 @@
-#!/usr/bin/which python3
+#!/usr/bin/env python3
+''' PE File Resource content identification and extraction of contents '''
 
-import pefile
-import sys
-import magic
 import argparse
+import pefile
+import magic
+
+__author__ = "Ben Mason"
+__copyright__ = "Copyright 2022"
+__version__ = "0.1.0"
+__email__ = "locutus@the-collective.net"
+__status__ = "Development"
 
 EXPORT_DATA = False
 IGNORE_NONE_NAMES = False
 FILE_SUFFIX = '.bin'
 
 def write_file(filename, bin_data):
-    file_handle = open(filename, 'wb')
-    file_handle.write(bin_data)
-    file_handle.close()
+    ''' Write Data to a file '''
+
+    with open(filename, 'wb') as file_handle:
+        file_handle.write(bin_data)
+
+    return 0
 
 def main(filename):
+    ''' Main function '''
 
     none_name_counter = 0
 
-    pe = pefile.PE(filename)
+    # Open PE file and read contents
+    thepefile = pefile.PE(filename)
+    all_data = thepefile.get_memory_mapped_image()
 
-    all_data = pe.get_memory_mapped_image()
-
-    print(pe.DIRECTORY_ENTRY_RESOURCE.struct)
+    print(thepefile.DIRECTORY_ENTRY_RESOURCE.struct)
     print ("-----------------------")
     print()
 
-    for entry in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+    for entry in thepefile.DIRECTORY_ENTRY_RESOURCE.entries:
 
         if entry.name is None and IGNORE_NONE_NAMES:
             print ("Skipping Directory entry named None")
@@ -40,7 +50,6 @@ def main(filename):
 
             for item_dir in item.directory.entries:
                 print(item_dir.data.struct)
-                # entry.directory.entries[0].directory.entries[0].data.struct
                 data_rva = item_dir.data.struct.OffsetToData
                 size = item_dir.data.struct.Size
 
@@ -67,18 +76,15 @@ def main(filename):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-
     parser.add_argument("file", help="Filename")
     parser.add_argument("-d", "--dump", help="Dump Contents to Files",
                     action="store_true")
     parser.add_argument("-i", "--ignore", help="Ignore Sections named None",
                     action="store_true")
-
     args = parser.parse_args()
 
     if args.dump:
         EXPORT_DATA = True
-
     if args.ignore:
         IGNORE_NONE_NAMES = True
 
